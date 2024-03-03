43c0caa9e96c823f6ad39648298df706cf5afaec
davidcastro1802 <157267631+davidcastro1802@users.noreply.github.com>
2024-03-03 02:57:05 -0600

    Add files via upload

---
+++ Project2/Week 3/Project2_Final.m
@@ -0,0 +1,16 @@
+% Define parameters
+radius = 200; % Radius of curved sections
+straight_length = 900; % Length of straight sections
+track_width = 15; % Width of the track
+
+path = struct();
+path.width = track_width;
+path.l_st = straight_length;
+path.radius = radius;
+
+simout = sim("p2_demo.slx");
+X = simout.X.Data;
+Y = simout.Y.Data;
+t = simout.tout;
+
+RACE = raceStat(X,Y,t,path);
